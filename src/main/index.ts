a404aeee7aa0a96b71f97c65eb49185f4cbf5a9c
nikany96 <nikanyardid96@hotmail.com>
2026-06-04 11:47:37 +0200

    Fix GPU crashes with OpenGL backend instead of desktop/EGL

--- src/main/index.ts
+++ src/main/index.ts
@@ -29,7 +29,8 @@ app.commandLine.appendSwitch('disable-translate');
 app.commandLine.appendSwitch('disable-spell-checking');
 // GTX 770 (nvidia-470xx) does not support Vulkan 1.3 required by Dawn/WebGPU
 app.commandLine.appendSwitch('disable-features', 'Vulkan');
-app.commandLine.appendSwitch('use-gl', 'desktop');
+app.commandLine.appendSwitch('use-gl', 'angle');
+app.commandLine.appendSwitch('use-angle', 'gl');
 
 export let mainWindow: BrowserWindow | null = null;
 
